7179c9d7954f502ba1c4c809843022b00f6887cc
tharindu97 <tharindukavishna@gmail.com>
2019-10-25 11:53:47 +0530

    16_part01

---
+++ weight_app/lib/ui/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState();
+      }
+    
+    }
+    
+class HomeState  extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Wieght on Planet X",),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+
+      backgroundColor: Colors.blueGrey,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new ListView(
+          padding: const EdgeInsets.all(2.5),
+          children: <Widget>[
+
+
+            new Image.asset('images/planet.png',
+            height: 133.0,
+            width: 200.0,
+            ),
+
+
+            new Container(
+                margin: const EdgeInsets.all(3.0),
+                alignment: Alignment.center,
+                child: new Column(
+                  children: <Widget>[
+
+
+                    new TextField(
+                      controller: null,
+                      keyboardType: TextInputType.number,
+                      decoration: new InputDecoration(
+                        labelText: 'Your Weight on Earth',
+                        hintText: 'In Pounds',
+                        icon: new Icon(Icons.person_outline)
+                      ),
+                    )
+                  ],
+                ),
+            )
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
