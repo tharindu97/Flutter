1eaa7bf3b0e00d2f7e8e5ae2894059856ddeb371
tharindu97 <tharindukavishna@gmail.com>
2019-11-15 15:49:49 +0530

    login_page_completed

--- project2/lib/login_page.dart
+++ project2/lib/login_page.dart
@@ -18,6 +18,74 @@ class LoginPage extends StatefulWidget{
       ),
 
     );
-    return new Container();
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: 'alucard@gmail.com',
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0)
+        )
+      ),
+
+    );
+
+     final password = TextFormField(
+      autofocus: false,
+      initialValue: 'some password',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0)
+        )
+      ),
+
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(32.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {},
+          color: Colors.lightBlueAccent,
+          child: Text('Log In', style: TextStyle(color: Colors.white),),
+        ),
+
+      ),
+    );
+
+    final forgotLabel = FlatButton(
+      child: Text('Forgot Password',style: TextStyle(color: Colors.black54),),
+      onPressed: () {},
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0,),
+            email,
+            SizedBox(height: 8.0,),
+            password,
+            SizedBox(height: 24.0,),
+            loginButton,
+            forgotLabel
+          ],
+        ),
+      ),
+    );
   }
 }
