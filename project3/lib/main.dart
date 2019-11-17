0972ab81c710bb2bd172b84da646edc615a8de17
tharindu97 <tharindukavishna@gmail.com>
2019-11-17 13:43:19 +0530

    Completed_project3

--- project3/lib/main.dart
+++ project3/lib/main.dart
@@ -11,8 +11,34 @@ class MyTextInput extends StatefulWidget{
   }
   
   class MyTextInputState  extends State<MyTextInput>{
+    final TextEditingController controller = new TextEditingController();
+    String result = "";
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Input Text"),backgroundColor: Colors.blue,),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new TextField(
+                decoration: new InputDecoration(
+                  hintText: "Type Here"
+                ),
+                onSubmitted: (String str){
+                  setState(() {
+                    result = result + "\n" + str;
+                  });
+                  controller.text = "";
+                },
+                controller: controller,
+              ),
+              new Text(result)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
